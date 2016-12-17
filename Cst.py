7ecd6c3edb0b8ec59c3ea601945dbd5d2a379507
chwuda <da.chenwu@student.umons.ac.be>
2016-12-17 21:26:25 +0100

    Update Cst.py

--- Cst.py
+++ Cst.py
@@ -6,7 +6,7 @@ class Cst(Expression):
 	#Class that represents a constant (ex: a constant inside a column in a table)
 
 
-	#On utilise un tuple pour representer la valeur et le type de cet valeur?
+	#On utilise un tuple pour representer la valeur et le type de cet valeur? ou pas besoin car on a specifié dans Attr?
 	def __init__(self, data):
 		self.data = data
 		
