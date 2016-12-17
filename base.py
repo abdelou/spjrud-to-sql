8f9bf562e7d8a9fe690f4766f83c77507d982c23
chwuda <da.chenwu@student.umons.ac.be>
2016-12-17 21:21:11 +0100

    Update base.py

--- base.py
+++ base.py
@@ -1,7 +1,11 @@
+import sqlite3
+
 class Relation(object):
 
-	#Class representing the relations (SPJRUD) or table (SQL)
-
+	#Class representing the relation (SPJRUD) or table (SQL)
+	
+	
+	#attr is a list of Attribute type objects
 	def __init__(self, name, attr):
 		self.name = name
 		self.attr = attr
@@ -13,10 +17,24 @@ class Relation(object):
 
 class Expression(object):
 
-	#Class used as a base for all type of SPJRUD expressions
+	#Class used as a base for all type of SPJRUD expressions (So, each Node of the AST)
 
 
 	def __init__(self, relation):
 		self.relation = relation
-
+		
+	
+class Attribute(object): #Object?
+	
+	#Class that represents the attributes (SPJRUD) or Columns (SQL)
+	
+	def __init__(self, name, type):
+		self.name = name
+		self.type = type
+		
+	def __str__(self):
+		return self.name
+	
+	def getType(self):
+		return self.type
 
