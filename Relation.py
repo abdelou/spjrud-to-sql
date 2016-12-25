e8f90a1766c0169db9202fb3651f83cbf1286631
Axtux <axtux@hotmail.com>
2016-12-25 20:18:12 +0100

    Add some functions

--- Relation.py
+++ Relation.py
@@ -14,6 +14,12 @@ class Relation :
     
     self.attributes = attributes
   
+  def getAttribute(self, attribute_name) :
+    for attr in self.attributes :
+      if attr.getName() == attribute_name :
+        return attr
+    raise Exception('Relation has no attribute named "'+attribute_name+'"')
+  
   def getAttributes(self) :
     return self.attributes
   
@@ -37,10 +43,23 @@ class Attribute :
       raise TypeError('Invalid type "'+type+'" for attribute "'+name+'"')
     self.type = type
   
+  def isComparableTo(self, other) :
+    if not isinstance(other, Attribute) :
+      return False
+    # numeric can be compared with numeric so only text cannot be compared with other
+    if self.getType() == 'TEXT' and other.getType() != 'TEXT' :
+      return False
+    if other.getType() == 'TEXT' and self.getType() != 'TEXT' :
+      return False
+    return True
+  
   def getName(self) :
     return self.name
   
   def getType(self) :
     return self.type
+  
+  def __str__(self) :
+    return 'Attribute "'+self.getName()+'" of type "'+self.getType()+'"'
 
 
