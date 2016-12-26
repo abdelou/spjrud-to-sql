7490957fe205ca8c407ffe968b7e3f104c2bc2db
chwuda <da.chenwu@student.umons.ac.be>
2016-12-26 01:27:19 +0100

    Comments

--- SelectAttribute.py
+++ SelectAttribute.py
@@ -1,6 +1,7 @@
 from Relation import Relation
 
 
+# Operator Select to access an attribute (or column)
 class SelectAttribute(Relation) :
   def __init__(self, attribute_name_1, attribute_name_2, subrelation) :
     # check and save subrelation
@@ -11,13 +12,15 @@ class SelectAttribute(Relation) :
     # schema is unchanged
     self.setAttributes(subrelation.getAttributes())
     
-    # check are made in getAttribute
+    # checks are made in getAttribute
     self.attribute1 = self.getAttribute(attribute_name_1)
     self.attribute2 = self.getAttribute(attribute_name_2)
     
+    # check that attribute1 and attribute2 are comparable
     if not self.attribute1.isComparableTo(self.attribute2) :
       raise Exception(str(self.attribute1)+' is not comparable with '+str(self.attribute2))
   
   def toSQL(self) :
     comparison = '"'+self.attribute1.getName()+'"="'+self.attribute2.getName()+'"'
+    # query
     return 'SELECT * FROM ('+self.subrelation.toSQL()+') WHERE '+comparison
