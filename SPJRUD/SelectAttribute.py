7b5bb4870c3cdead4fd23aa6253fae849929d9d5
Axtux <axtux@hotmail.com>
2016-12-26 17:39:45 +0100

    better error message

--- SPJRUD/SelectAttribute.py
+++ SPJRUD/SelectAttribute.py
@@ -6,19 +6,18 @@ from functions import *
 class SelectAttribute(Relation) :
   def __init__(self, attribute_name_1, attribute_name_2, subrelation) :
     # check and save subrelation
-    check_relation(subrelation, 3)
-    self.subrelation = subrelation
+    self.subrelation = check_relation(self, subrelation, 'subrelation')
     
     # schema is unchanged
     self.setAttributes(subrelation.getAttributes())
     
     # checks are made in getAttribute
-    self.attribute1 = self.getAttribute(attribute_name_1)
-    self.attribute2 = self.getAttribute(attribute_name_2)
+    self.attribute1 = self.getAttribute(attribute_name_1, True, 'subrelation')
+    self.attribute2 = self.getAttribute(attribute_name_2, True, 'subrelation')
     
     # check that attribute1 and attribute2 are comparable
     if not self.attribute1.isComparableTo(self.attribute2) :
-      raise Exception(str(self.attribute1)+' is not comparable with '+str(self.attribute2))
+      self.error(str(self.attribute1)+' is not comparable with '+str(self.attribute2))
   
   def toSQL(self) :
     comparison = '"'+self.attribute1.getName()+'"="'+self.attribute2.getName()+'"'
