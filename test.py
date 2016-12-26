c7d5602d848ce19a6e5ad2e3de8711b79b8057f1
Axtux <axtux@hotmail.com>
2016-12-26 18:12:05 +0100

    run different tests from command line

--- test.py
+++ test.py
@@ -9,81 +9,97 @@ from SPJRUD.Join import Join
 from SPJRUD.Rename import Rename
 from SPJRUD.Union import Union
 from SPJRUD.Difference import Difference
+# SQLite
+from sqlite import SQLiteRelation
+# for arguments
+import sys
 
-# create a test database
+l = len(sys.argv)
+if l < 2 :
+  print "Usage : python test.py TEST_NUMBER"
+  exit(0)
+try :
+  test = int(sys.argv[1])
+except ValueError :
+  print "Usage : python test.py TEST_NUMBER"
+  exit(0)
+
+# create an SQLite test database and get tables emp and dept
 DB_NAME = 'test_db'
 create_database(DB_NAME)
-
-#""" Test SQLiteRelation
-from sqlite import SQLiteRelation
-
 emp = SQLiteRelation(DB_NAME, 'emp')
 dept = SQLiteRelation(DB_NAME, 'dept')
-#exec_request(DB_NAME, emp)
-#exec_request(DB_NAME, dept)
-#"""
-
-""" Test SelectAttribute
-# not comparable attributes
-#rel = SelectAttribute('ename', 'empno', emp)
-
-# comparable attributes
-rel = SelectAttribute('ename', 'job', emp)
-
-exec_request(DB_NAME, rel)
-#"""
 
 
-""" Test SelectConstant
-# comparables attributes
-rel = SelectConstant('ename', 'BLAKE', emp)
-
-exec_request(DB_NAME, rel)
-#"""
-
-
-""" Test Project
-rel = Project(['ename', 'empno'], emp)
-
-exec_request(DB_NAME, rel)
-#"""
+# test SQLiteRelation
+if test == 1 :
+  rel = emp
+if test == 2 :
+  rel = dept
 
 
-""" Test Join
-rel = SelectConstant('ename', 'BLAKE', emp)
-
-#rel = Join(rel, dept)
-rel = Join(rel, Rename('deptno', 'deptno2', dept))
-
-exec_request(DB_NAME, rel)
-#"""
-
-
-""" Test Rename
-rel = Rename('ename', 'Name', emp)
-exec_request(DB_NAME, rel)
-#"""
+# Test SelectAttribute
+if test == 10 :
+  # error : attribute does not exists
+  rel = SelectAttribute('ename', 'nonexistant', emp)
+if test == 11 :
+  # error : not comparable attributes
+  rel = SelectAttribute('ename', 'empno', emp)
+if test == 12 :
+  # working : comparable existing attributes
+  rel = SelectAttribute('ename', 'job', emp)
 
 
-""" Test Union
-rel1 = SelectConstant('ename', 'BLAKE', emp)
-rel2 = SelectConstant('ename', 'JONES', emp)
-
-rel = Union(rel1, rel2)
-
-exec_request(DB_NAME, rel)
-#"""
-
-
-#""" Test Difference
-rel1 = SelectConstant('ename', 'BLAKE', emp)
-#rel2 = SelectConstant('ename', 'JONES', emp)
-
-rel = Difference(emp, rel1)
-
-exec_request(DB_NAME, rel)
-#"""
+# Test SelectConstant
+if test == 20 :
+  # error : attribute does not exists
+  rel = SelectAttribute('nonexistant', 'my_constant', emp)
+if test == 21 :
+  # working : existing attribute
+  rel = SelectConstant('ename', 'BLAKE', emp)
 
 
+# Test Project
+if test == 30 :
+  # error : attribute does not exists
+  rel = Project(['ename', 'nonexistant'], emp)
+if test == 31 :
+  # working : existing attribute
+  rel = Project(['ename', 'empno'], emp)
+
+# Test Join
+if test == 40 :
+  # error : 2 attributes with same name but different types
+  rel1 = Rename('ename', 'common', emp)
+  rel2 = Rename('empno', 'common', emp)
+  rel = Join(rel1, rel2)
+if test == 41 :
+  #rel = Join(rel, dept)
+  rel = Join(rel, Rename('deptno', 'deptno2', dept))
+
+
+# Test Rename
+if test == 50 :
+  rel = Rename('ename', 'Name', emp)
+
+
+# Test Union
+if test == 60 :
+  rel1 = SelectConstant('ename', 'BLAKE', emp)
+  rel2 = SelectConstant('ename', 'JONES', emp)
+  rel = Union(rel1, rel2)
+
+
+# Test Difference
+if test == 70 :
+  rel1 = SelectConstant('ename', 'BLAKE', emp)
+  rel2 = Rename('ename', 'Nom', emp)
+
+  rel = Difference(emp, rel2)
+
+# execute test request and delete database
+try :
+  exec_request(DB_NAME, rel)
+except NameError :
+  print 'No test with number "'+str(test)+'"'
 delete_database(DB_NAME)
-
